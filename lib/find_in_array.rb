c549977ea71b9c50f5c2ee2ddf5eec14bc23e3f1
artificial-composition-7121 <tbutler1132@gmail.com>
2020-07-20 20:57:45 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
+
   counter = 0
 
   while counter < array.length do
-      array.index(value_to_find)
+      array.index(2)
       counter += 1
   end# Add your solution here
 
